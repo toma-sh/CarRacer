fb84212acf4533a34f257719d324c374a4037568
toma-sh <zoltom83@gmail.com>
2022-10-12 20:46:05 +0200

    Start project

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+import time
+import math
+
+GRASS = pygame.image.load("imgs/grass.png")
+TRACK = pygame.image.load("imgs/track.png")
+
+TRACK_BORDER = pygame.image.load("imgs/track-border.png")
+FINISH = pygame.image.load("imgs/finish.png")
+
+RED_CAR = pygame.image.load("imgs/red-car.png")
+GREEN_CAR = pygame.image.load("imgs/green-car.png")
+
