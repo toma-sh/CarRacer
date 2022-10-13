bf3fac4150c7e4804c37ef204dee5aeb840882e1
toma-sh <zoltom83@gmail.com>
2022-10-14 00:43:52 +0200

    Add full code of moving a cae

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import imghdr
 import pygame
 import time
 import math
 
-from utils import scale_img
+from utils import scale_img, blit_rotate_center
 
 GRASS = scale_img(pygame.image.load("imgs/grass.jpg"), 2.5)
 
@@ -18,49 +19,96 @@ WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 pygame.display.set_caption("Racing Game!")
+#-------------------------------------------------------------------------------------
 
 FPS = 60
-
+################## Classes #################
 class AbstractCar:
+    
+
     def __init__(self, max_vel, rotation_vel):
+        self.img = self.IMG
         self.max_vel = max_vel
         self.vel = 0
         self.rotation_vel = rotation_vel
         self.angle = 0
+        self.x, self.y = self.START_POS
+        self.acceleration = 0.1
     
     def rotate(self, left=False, right=False):
         if left:
-            self.angle += self. rotation_vel
+            self.angle += self.rotation_vel
         elif right:
-            self.angle -= self. rotation_vel
+            self.angle -= self.rotation_vel
 
-   
+    def draw(self, win):
+        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
 
- 
+    def move_forward(self):
+        self.vel = min(self.vel+self.acceleration, self.max_vel)
+        self.move()
+
+    def move(self):
+        radians = math.radians(self.angle)
+        vertical = math.cos(radians) * self.vel
+        horizontal = math.sin(radians) * self.vel
+
+        self.y -= vertical
+        self.x -= horizontal
+
+    def reduce_speed(self):
+        self.vel= max(self.vel - self.acceleration/2, 0)
+        self.move()
 
 
 
-def draw(win, images):
+
+class PlayerCar(AbstractCar):
+    IMG = RED_CAR
+    START_POS = (180,200)
+
+################# FUNCTIONS ####################
+
+def draw(win, images, player_car):
     for img, pos in images:
         win.blit(img, pos)
+    
+    player_car.draw(win)
+    pygame.display.update()
 
+#################################################
+
+################ MAIN APP ####################
 
 clock = pygame.time.Clock()
 
 images=[(GRASS, (0,0)),(TRACK, (0,0))]
 run = True
+player_car = PlayerCar(4,4)
 
 while run:
     clock.tick(FPS)
 
-    draw(WIN, images)
+    draw(WIN, images, player_car)
     
-    pygame.display.update()
-
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run=False
             break
+    
+    keys = pygame.key.get_pressed()
+    moved = False
+
+    if keys[pygame.K_a]:
+        player_car.rotate(left=True)
+    if keys[pygame.K_d]:
+        player_car.rotate(right=True)
+    if keys[pygame.K_w]:
+        moved = True
+        player_car.move_forward()
+
+    if not moved:
+        player_car.reduce_speed()
+
 
 pygame.quit()
