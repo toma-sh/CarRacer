f2caf8f719c96e5c690de29443663d4baeafd7de
toma-sh <zoltom83@gmail.com>
2022-10-14 21:24:30 +0200

    collisions

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ GRASS = scale_img(pygame.image.load("imgs/grass.jpg"), 2.5)
 
 TRACK = scale_img(pygame.image.load("imgs/track.png"),0.9)
 TRACK_BORDER = scale_img(pygame.image.load("imgs/track-border.png"), 0.9)
+TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
 
 FINISH = pygame.image.load("imgs/finish.png")
 
@@ -48,6 +49,10 @@ class AbstractCar:
         self.vel = min(self.vel+self.acceleration, self.max_vel)
         self.move()
 
+    def move_backward(self):
+        self.vel = max(self.vel-self.acceleration, -self.max_vel/2)
+        self.move()
+
     def move(self):
         radians = math.radians(self.angle)
         vertical = math.cos(radians) * self.vel
@@ -56,16 +61,25 @@ class AbstractCar:
         self.y -= vertical
         self.x -= horizontal
 
-    def reduce_speed(self):
-        self.vel= max(self.vel - self.acceleration/2, 0)
-        self.move()
+    def collide(self, mask, x=0, y=0):
+        car_mask = pygame.mask.from_surface(self.img)
+        offset = (int(self.x-x), int(self.y-y))
+        poi = mask.overlap(car_mask, offset)
 
-
+        return poi
 
 
 class PlayerCar(AbstractCar):
     IMG = RED_CAR
     START_POS = (180,200)
+
+    def reduce_speed(self):
+        self.vel= max(self.vel - self.acceleration/2, 0)
+        self.move()
+
+    def bounce(self):
+        self.vel = -self.vel/2
+        self.move()
 
 ################# FUNCTIONS ####################
 
@@ -76,6 +90,24 @@ def draw(win, images, player_car):
     player_car.draw(win)
     pygame.display.update()
 
+def move_player(player_car):
+    keys = pygame.key.get_pressed()
+    moved = False
+
+    if keys[pygame.K_a]:
+        player_car.rotate(left=True)
+    if keys[pygame.K_d]:
+        player_car.rotate(right=True)
+    if keys[pygame.K_w]:
+        moved = True
+        player_car.move_forward()
+    if keys[pygame.K_s]:
+        moved = True
+        player_car.move_backward()
+
+    if not moved:
+        player_car.reduce_speed()
+
 #################################################
 
 ################ MAIN APP ####################
@@ -84,7 +116,7 @@ clock = pygame.time.Clock()
 
 images=[(GRASS, (0,0)),(TRACK, (0,0))]
 run = True
-player_car = PlayerCar(4,4)
+player_car = PlayerCar(6,4)
 
 while run:
     clock.tick(FPS)
@@ -96,19 +128,10 @@ while run:
             run=False
             break
     
-    keys = pygame.key.get_pressed()
-    moved = False
+    move_player(player_car)
 
-    if keys[pygame.K_a]:
-        player_car.rotate(left=True)
-    if keys[pygame.K_d]:
-        player_car.rotate(right=True)
-    if keys[pygame.K_w]:
-        moved = True
-        player_car.move_forward()
-
-    if not moved:
-        player_car.reduce_speed()
+    if player_car.collide(TRACK_BORDER_MASK) != None:
+        player_car.bounce()
 
 
 pygame.quit()
